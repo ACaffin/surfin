780ff5f71513790773c1ec67e8d9d18586df1d2b
Antoine Caffin <caffinantoine@gmail.com>
2017-10-28 15:44:10 +0000

    modification des vues de Devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  #critère de validation qui dit que le full name ne doit pas fiare puls de 50 caractères
+  validates :fullname, presence:true, length: {maximum: 50}
 end

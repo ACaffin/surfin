ca81ed1f8dbb54677c190fb8e6bc86036f20265f
Antoine Caffin <caffinantoine@gmail.com>
2017-10-28 16:25:49 +0000

    ajout photo de profil

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ActiveRecord::Base
 
   #critère de validation qui dit que le full name ne doit pas fiare puls de 50 caractères
   validates :fullname, presence:true, length: {maximum: 50}
+  
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/assets/default_image.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+
 end

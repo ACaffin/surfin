e33c5df64469009652d1bafafe9683a60db33d31
Antoine Caffin <caffinantoine@gmail.com>
2017-10-29 18:12:06 +0000

    modif controller photo

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -2,6 +2,7 @@ class LessonsController < ApplicationController
 
 before_action :set_lesson, only: [:show, :edit, :update]
 before_action :authenticate_user!, except: [:show]
+before_action :require_same_user, only: [:edit, :update]
 
     def index
         @lessons = current_user.lessons
@@ -14,21 +15,35 @@ before_action :authenticate_user!, except: [:show]
     def create
         @lesson = current_user.lessons.build(lesson_params) 
         if @lesson.save
-            redirect_to @lesson, notice:"Votre annonce a été ajoutée avec succès"
+            if params[:images]
+                params[:images].each do |i|
+                    @lesson.photos.creat(image: i)
+                end
+            end
+            @photos = @lesson.photos
+            redirect_to @edit_lesson_path(@lesson), notice:"Votre annonce a été ajoutée avec succès"
         else
             render :new
         end
     end
     
     def show
+        @photos = @lesson.photos
     end
     
     def edit
+        @photos = @lesson.photos
     end
     
     def update
         if @lesson.update(lesson_params)
-            redirect_to @lesson, notice:"Modification enregistrée..."
+            if params[:images]
+                params[:images].each do |i|
+                    @lesson.photos.creat(image: i)
+                end
+            end
+            @photos = @lesson.photos
+            redirect_to @edit_lesson_path(@lesson), notice:"Modification enregistrée..."
         else
             render :edit
         end
@@ -42,5 +57,13 @@ private
     def lesson_params
        params.require(:lesson).permit(:lesson_level, :lesson_duration, :price, :is_equipment, :active) 
     end
+    
+    def require_same_user
+       if current_user.id 8= @lesson.user_id
+           flash[:danger] = "Vous n'avez pas le droit de modifier cette page"
+           redirect_to root_path
+       end
+    end
+    
     
 end

9ec9f8a8473f5e51ab6fdb1d67df4087c917e047
Antoine Caffin <caffinantoine@gmail.com>
2017-10-30 10:47:22 +0000

    test3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,7 @@ Rails.application.configure do
     :storage=>:cloudinary,
     :patch=>':id/:style/:filename'
   })
+  
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.

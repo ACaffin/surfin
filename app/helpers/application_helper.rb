88c25e25c503687602e3dae926657fa85f3a2629
Antoine Caffin <caffinantoine@gmail.com>
2017-10-30 10:42:15 +0000

    test2

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,11 @@
 module ApplicationHelper
 
     def avatar_url(user)
-        user.avatar.url 
+        if user.avatar.present?
+            user.vatar.url
+        else
+            'default_image.png'
+        end
     end
 
 end
